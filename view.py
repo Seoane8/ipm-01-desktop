5343064ec93d7d2d6894865e7d8f76be32d6399f
GuilleSSM <g.somoza@udc.es>
2020-10-23 15:25:33 +0200

    Añadida funcionalidad a view

--- view.py
+++ view.py
@@ -12,23 +12,80 @@ from gi.repository import Gtk
 
 class View:
 
-    def main():
-        pass
+    def main(cls):
+    	Gtk.main()
 
     def buil_view(self):
-        pass
+        interv_active = None
+		ad_active = None
+		url = "http://127.0.0.1:5000/"
+		notes = [_('do'),_('do♯/re♭'),_('re'),_('re♯/mi♭'),_('mi'),_('fa')
+				,_('fa♯/sol♭'),_('sol'),_('sol♯/la♭'),_('la'),_('la♯/si♭'),_('si')]
+		intervals = {}
+
+		def __init__(self):
+			Gtk.Window.__init__(self, title=_("Intervalos"))
+
+			self.set_default_size(500,400)
+			self.set_border_width(10)
+
+			vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+			hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+			self.spinner = Gtk.Spinner()
+
+			self.intervals_combo = Gtk.ComboBoxText()
+			self.intervals_combo.set_entry_text_column(0)
+			self.intervals_combo.connect("changed", self.connect_intervals_combo_changed)
+			threading.Thread(target=self.get_intervals, daemon=True).start()
+			hbox.pack_start(self.intervals_combo, True, True, 0)
+
+			# Create Asc and Des buttons
+			button = Gtk.ToggleButton(label = "Asc")
+			button.connect("toggled", self.connect_dir_button_togled, "Asc")
+			hbox.pack_start(button, True, True, 10)
+			button = Gtk.ToggleButton(label = "Des")
+			button.connect("toggled", self.connect_dir_button_togled, "Des")
+			hbox.pack_start(button, True, True, 0)
+
+			self.tittle = Gtk.Label(label = "")
+
+			self.notes_distance = Gtk.Label(label = "")
+
+			self.songs_liststore = Gtk.ListStore(str, str, str)
+
+			# Create TreeView with songs list as model
+			tree = Gtk.TreeView(self.songs_liststore)
+			renderer = Gtk.CellRendererText()
+			column = Gtk.TreeViewColumn(_("Titulo"), renderer, markup=0)
+			tree.append_column(column)
+			column = Gtk.TreeViewColumn(_("Favorita"), renderer, text=2)
+			tree.append_column(column)
+
+			# Select row action
+			selection = tree.get_selection()
+			selection.connect("changed", self.connect_link_selection)
+
+			vbox.add(hbox)
+			vbox.pack_start(self.spinner, False, False, 10)
+			vbox.pack_start(self.tittle, False, False, 10)
+			vbox.pack_start(self.notes_distance, False, False, 10)
+			vbox.add(tree)
+
+			self.add(vbox)
 
     def show_all(self):
-        pass
+        self.win.show_all()
 
     def connect_dir_button_togled(self, fun):
-        pass
+		self.win.connect('on_dir_button_toggled', fun)
 
     def connect_interval_combo_changed(self, fun):
-        pass
+		self.win.connect('on_intervals_combo_changed', fun)
 
     def connect_link_selection(self, fun):
-        pass
+        self.win.connect('on_link_selection', fun)
+
 
     def show_waiting(self, b):
         if b:
@@ -36,8 +93,15 @@ class View:
         else:
             self.spinner.stop()
 
+
     def show_error(self):
-        pass
+		dialog = Gtk.MessageDialog(parent = self,
+									message_type = Gtk.MessageType.ERROR,
+									buttons = Gtk.ButtonsType.CLOSE,
+									text = _("Error en la conexión con la red, inténtelo más tarde"))
+		dialog.run()
+		dialog.destroy()
+		Gtk.main_quit()
 
     def show_need_entry(self):
         self.tittle.set_label("")
